2bfc44f5650ab2d711a2e932e5629b6cb4dcf36b
cyteon <129582290+Cyteon@users.noreply.github.com>
2024-11-11 15:00:58 +0100

    fix bored

--- src/commands/fun/bored.js
+++ src/commands/fun/bored.js
@@ -1,22 +1,51 @@
 import { SlashCommandBuilder } from "discord.js";
 
 const data = new SlashCommandBuilder()
-    .setName("bored")
-    .setDescription("Get a random activity to do")
-    .setIntegrationTypes(0, 1)
-    .setContexts(0, 1, 2);
+  .setName("bored")
+  .setDescription("Get a random activity to do")
+  .setIntegrationTypes(0, 1)
+  .setContexts(0, 1, 2);
 
 const execute = async function (interaction) {
-    const response = await fetch("https://bored-api.appbrewery.com/random");
+  const response = await fetch("https://bored-api.appbrewery.com/random");
+
+  if (response.ok) {
     const data = await response.json();
 
     await interaction.reply({
-        embeds: [
+      embeds: [
+        {
+          title: data.activity,
+          color: 0x56b3fa,
+          fields: [
             {
-                title: data.activity,
-                description: data.type,
-                color: 0x56b3fa
-            }
-        ]
+              name: "Type",
+              value: data.type.charAt(0).toUpperCase() + data.type.slice(1),
+              inline: true,
+            },
+            {
+              name: "Participants",
+              value: data.participants,
+              inline: true,
+            },
+            {
+              name: "Price",
+              value:
+                data.price == 0
+                  ? "Free"
+                  : "$".repeat((data.price * 5).toFixed(0)),
+              inline: true,
+            },
+          ],
+        },
+      ],
     });
-}
+  } else {
+    await interaction.reply({
+      content: "Failed to fetch a random activity",
+      ephemeral: true,
+    });
+  }
+};
+
+export default { data, execute };
