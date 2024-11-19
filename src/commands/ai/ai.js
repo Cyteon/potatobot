f7c84dd052980da014f59e584985440145c459ce
cyteon <129582290+Cyteon@users.noreply.github.com>
2024-11-19 17:56:29 +0100

    llama 3.2 90b text preview is deprecated

--- src/commands/ai/ai.js
+++ src/commands/ai/ai.js
@@ -42,7 +42,7 @@ const replaceList = [
 ];
 
 const models = [
-  "llama-3.2-90b-text-preview",
+  "llama-3.2-11b-text-preview",
   "llama-3.1-70b-versatile",
   "llama-3.1-8b-instant",
   "llama3-groq-70b-8192-tool-use-preview",
