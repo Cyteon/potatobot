75f474511ecf031736ac57cc52cc0b2b066588c2
cyteon <129582290+Cyteon@users.noreply.github.com>
2024-11-19 18:00:53 +0100

    remove super slow model

--- src/commands/ai/ai.js
+++ src/commands/ai/ai.js
@@ -43,7 +43,6 @@ const replaceList = [
 
 const models = [
   "llama-3.2-11b-text-preview",
-  "llama-3.1-70b-versatile",
   "llama-3.1-8b-instant",
   "llama3-groq-70b-8192-tool-use-preview",
   "llama3-groq-8b-8192-tool-use-preview",
