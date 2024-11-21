b69adcbed419fb96ca9c8f4245201417559c25dc
cyteon <129582290+Cyteon@users.noreply.github.com>
2024-11-21 17:32:26 +0100

    fix daily

--- src/commands/economy/eco/daily.js
+++ src/commands/economy/eco/daily.js
@@ -1,34 +1,40 @@
 import { SlashCommandSubcommandBuilder } from "discord.js";
 import EconomyUser from "../../../lib/models/EconomyUser.js";
-import random from "random"
+import random from "random";
 
 const data = new SlashCommandSubcommandBuilder()
-    .setName("daily")
-    .setDescription("Claim your daily reward!")
+  .setName("daily")
+  .setDescription("Claim your daily reward!");
 
 const execute = async function (interaction) {
-    let user = await EconomyUser.findOne({ id: interaction.user.id }).cache("1 minute");
+  let user = await EconomyUser.findOne({ id: interaction.user.id }).cache(
+    "1 minute",
+  );
 
-    if (!user) {
-        user = await EconomyUser.create({
-            id: interaction.user.id,
-            balance: 50
-        });
-    }
+  if (!user) {
+    user = await EconomyUser.create({
+      id: interaction.user.id,
+      balance: 50,
+    });
+  }
 
-    let lastDaily = user.lastDaily || 0;
+  let lastDaily = user.lastDaily || 0;
 
-    if (Date.now() - lastDaily < 86400000) {
-        return await interaction.reply(`🕒 You have already claimed your daily reward! Try again <t:${(Date.now() / 1000 + 86400).toFixed(0)}:R>`);
-    }
+  if (Date.now() - lastDaily < 86400000) {
+    return await interaction.reply(
+      `🕒 You have already claimed your daily reward! Try again <t:${Math.floor(lastDaily / 1000) + 86400}:R>`,
+    );
+  }
 
-    const rand = random.int(100, 500);
-    
-    user.balance += rand;
-    user.lastDaily = Date.now();
-    await user.save();
+  const rand = random.int(100, 500);
 
-    await interaction.reply(`💵 You have claimed your daily reward and got $${rand}!`);
-}
+  user.balance += rand;
+  user.lastDaily = Date.now();
+  await user.save();
 
-export default { data, execute }
+  await interaction.reply(
+    `💵 You have claimed your daily reward and got $${rand}!`,
+  );
+};
+
+export default { data, execute };
